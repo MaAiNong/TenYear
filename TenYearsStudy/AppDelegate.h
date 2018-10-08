69d148e4724d31a0e591b69417c63a9543afb0d3
sunuo <xianyirenxunuo@gmail.com>
2018-10-08 21:58:13 +0800

    Initial Commit

---
+++ TenYearsStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TenYearsStudy
+//
+//  Created by kevin on 2018/10/8.
+//  Copyright © 2018年 徐诺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
